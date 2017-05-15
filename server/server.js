c24f4a3c9cd761e4c7bb61da4e097e53969150e0
Dominic Trang <dominic_t@hotmail.co.nz>
2017-05-15 15:55:11 +1200

    docker-compose lib

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 var app  	   = require('express')();
 var http	   = require('http').Server(app);
 var exec 	   = require('child_process').exec;
+var DockerCompose = require("docker-compose-remote-api");
+var docker = DockerCompose({cwd: '~/gateway_nodejs'}).DockerRemoteAPI({
+    host: '127.0.0.1',
+    port: 2375
+});
 
 app.get('/', function(req, res) {
 
@@ -16,9 +21,18 @@ app.get('/', function(req, res) {
 // kitset_db_mongo
 
 app.get('/mysql/start', function(req, res) {
-	exec('cd ~/gateway_nodejs && docker-compose start -d mysql; touch ~/docker-has-started.txt', 
-		  {timeout: 60000});
-	res.send('Start mysql');
+	// exec('cd ~/gateway_nodejs && docker-compose start -d mysql; touch ~/docker-has-started.txt');
+	// res.send('Start mysql');
+	docker.getContainerId("kitset_db_mysql", function(err, id){
+	    if(err) console.log(err);
+	    else{
+	        var container = docker.dockerode.getContainer(id);
+	        container.inspect(function (err, data) {
+	            console.log(data);
+	        });
+	    }
+	});
+
 });
 
 app.get('/mysql/stop', function(req, res) {
@@ -30,8 +44,6 @@ app.get('/myql/restart', function(req, res) {
 	exec('cd ~/gateway_nodejs && docker-compose restart mysql');
 	res.send('Restart mysql');
 });
-
-
 
 
 
